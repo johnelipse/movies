657dbebf0c14c89649f3a044e3b978d071ca78a8
johnelipse <johnelipse472@gmail.comgit config --global user.name johnelipseOmit --globalgit commit -meventgit config --global user.email johnelipse472@gmail.com>
2024-06-14 04:29:52 -0700

    update

--- app/libs/mongodb.js
+++ app/libs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export default async function connectMongodb(){
 try {
- await mongoose.connect(process.env.MONGO_URI)  
+ await mongoose.connect('process'.env.MONGO_URI)  
  console.log("connected to mongodb")
 } catch (error) {
     console.log(error)
